8276dff8929cbff8e9460b261d00affc2b8af094
Daniel Hüsken <mail@danielhuesken.de>
2018-11-15 13:05:49 +0100

    change uninstall file loading

--- uninstall.php
+++ uninstall.php
@@ -7,7 +7,10 @@ use Inpsyde\AGBConnector\Plugin;
 if (! defined('WP_UNINSTALL_PLUGIN')) {
     die();
 }
-require_once __DIR__ . '/src/Plugin.php';
+
+if (! class_exists('Inpsyde\AGBConnector\Plugin')) {
+    require_once __DIR__ . '/src/Plugin.php';
+}
 
 delete_option(Plugin::OPTION_USER_AUTH_TOKEN);
 delete_option(Plugin::OPTION_TEXT_ALLOCATIONS);
