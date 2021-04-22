35ed6507a521051c87de053a7215ac75ce8ec039
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-04-22 18:08:47 +0300

    implement XML-based Document factory

---
+++ src/Document/Factory/XmlBasedDocumentFactory.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Inpsyde\AGBConnector\Document\Factory;
+
+use Inpsyde\AGBConnector\Document\Document;
+use Inpsyde\AGBConnector\Document\DocumentInterface;
+use Inpsyde\AGBConnector\Document\Map\XmlMetaFields;
+use SimpleXMLElement;
+
+class XmlBasedDocumentFactory implements XmlBasedDocumentFactoryInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function createDocument(SimpleXMLElement $xml): DocumentInterface
+    {
+        return new Document(
+           $this->getTag(XmlMetaFields::XML_FIELD_TITLE, $xml),
+           $this->getTag(XmlMetaFields::XML_FIELD_TEXT_CONTENT, $xml),
+           $this->getTag(XmlMetaFields::XML_FIELD_HTML_CONTENT, $xml),
+           $this->getTag(XmlMetaFields::XML_FIELD_COUNTRY, $xml),
+           $this->getTag(XmlMetaFields::XML_FIELD_LANGUAGE, $xml),
+           $this->getTag(XmlMetaFields::XML_FIELD_PDF_URL, $xml)
+        );
+    }
+
+    /**
+     * Get content of the top-level XML tag, empty string if tag not found.
+     *
+     * @param string $tagName Tag name to get.
+     * @param SimpleXMLElement $xml XML to search in.
+     *
+     * @return string
+     */
+    protected function getTag(string $tagName, SimpleXMLElement $xml): string
+    {
+        return $xml->offsetExists($tagName) ? (string) $xml->offsetGet($tagName) : '';
+    }
+}
