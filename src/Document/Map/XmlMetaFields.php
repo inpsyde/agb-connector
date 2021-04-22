a90ccea1aa03b045635b1fe98b1c5d8aac25c1a9
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-04-22 17:34:21 +0300

    add document fields map classes

---
+++ src/Document/Map/XmlMetaFields.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Inpsyde\AGBConnector\Document\Map;
+
+/**
+ * Class XmlMetaFields
+ *
+ * @package Inpsyde\AGBConnector\Document\Map
+ *
+ * The list of XML fields containing document data.
+ */
+class XmlMetaFields
+{
+    const XML_FIELD_TITLE = 'rechtstext_title';
+
+    const XML_FIELD_HTML_CONTENT = 'rechtstext_html';
+
+    const XML_FIELD_TEXT_CONTENT = 'rechtstext_text';
+
+    const XML_FIELD_COUNTRY  = 'rechtstext_country';
+
+    const XML_FIELD_LANGUAGE = 'rechtstext_language';
+
+    const XML_FIELD_PDF_URL = 'rechtstext_pdf_url';
+
+    const XML_FIELD_PDF_MD5_HASH = 'rechtstext_pdf_md5hash';
+}
