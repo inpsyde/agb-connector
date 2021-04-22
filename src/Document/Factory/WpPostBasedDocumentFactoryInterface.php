edff2db9802198c5267f24180abafc0485129308
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-04-22 17:24:07 +0300

    add Document factories interfaces

---
+++ src/Document/Factory/WpPostBasedDocumentFactoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Inpsyde\AGBConnector\Document\Factory;
+
+use Inpsyde\AGBConnector\CustomExceptions\XmlApiException;
+use Inpsyde\AGBConnector\Document\DocumentInterface;
+use WP_Post;
+
+/**
+ * Interface WpPostBasedDocumentFactoryInterface
+ *
+ * @package Inpsyde\AGBConnector\Document\Factory
+ *
+ * Service able to create DocumentInterface instance from WP post.
+ */
+interface WpPostBasedDocumentFactoryInterface
+{
+    /**
+     * Create a new document from WP post.
+     *
+     * @param WP_Post $post The WP post to create document from.
+     *
+     * @return DocumentInterface
+     *
+     * @throws XmlApiException If couldn't create a new instance.
+     */
+    public function createDocument(WP_Post $post): DocumentInterface;
+
+}
