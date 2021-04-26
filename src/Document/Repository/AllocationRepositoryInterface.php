b762d288ad6f9745cfd5912b8059b395dd0bcfd7
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-04-26 20:01:35 +0300

    update allocation repository interface

--- src/Document/Repository/AllocationRepositoryInterface.php
+++ src/Document/Repository/AllocationRepositoryInterface.php
@@ -20,9 +20,22 @@ interface AllocationRepositoryInterface
     /**
      * Get the allocation for the given document type, country and language.
      *
+     * @param string $type
+     * @param string $country
+     * @param string $language
+     *
      * @return DocumentAllocationInterface|null
      */
-    public function getByTypeCountryAndLanguage(): ?DocumentAllocationInterface;
+    public function getByTypeCountryAndLanguage(string $type, string $country, string $language): ?DocumentAllocationInterface;
+
+    /**
+     * Get all document allocations of the given type.
+     *
+     * @param string $type
+     *
+     * @return array
+     */
+    public function getAllOfType(string $type): array;
 
     /**
      * Save the allocation to the DB.
