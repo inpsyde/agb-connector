547a361982ef13111ee0da8b100c37ef7beb9abb
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-04-29 09:16:24 +0300

    bare implement document page finder

---
+++ src/Document/DocumentPageFinder/DocumentPageFinder.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Inpsyde\AGBConnector\Document\DocumentPageFinder;
+
+
+class DocumentPageFinder implements DocumentFinderInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    function findPagesDisplayingDocument(int $documentId): array
+    {
+        return [];
+        // TODO: Implement findPagesDisplayingDocument() method.
+    }
+}
