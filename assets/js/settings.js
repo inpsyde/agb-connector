9fdbac3c62f497359c4c91496371317c34e006b3
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-05-17 13:16:55 +0300

    send AJAX request on changing document settings

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,5 +1,23 @@
 jQuery(document).ready(function($){
-    /*$('.button-link.editinline').on('click', function (e){
-        console.log(e.target);
-    });*/
+    $('table td input[type="checkbox"].agb-document-settings').on('change', function(e){
+        let documentId = parseInt( $( $(e.target).closest('tr')).attr('id').substr(5) );
+        let fieldName = $(e.target).attr('name');
+        let value = $(e.target).is(':checked') ? '1' : '0';
+
+        $.post(
+            {
+                url: ajaxurl,
+                data: {
+                    documentId: documentId,
+                    nonce: agbConnectorSettings.nonce,
+                    fieldName: fieldName,
+                    value: value
+                },
+                dataType: 'json',
+                onSuccess: function (response) {
+                    agbConnectorSettings.nonce = response.nonce;
+                }
+            }
+        );
+    });
 });
