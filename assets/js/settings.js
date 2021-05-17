283f22f4b4ba41d7ea2c4704287b57140ed67081
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-05-17 14:42:48 +0300

    rename request parameter

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
     $('table td input[type="checkbox"].agb-document-settings').on('change', function(e){
         let documentId = parseInt( $( $(e.target).closest('tr')).attr('id').substr(5) );
         let fieldName = $(e.target).attr('name');
-        let value = $(e.target).is(':checked') ? '1' : '0';
+        let fieldValue = $(e.target).is(':checked') ? '1' : '0';
 
         $.post(
             {
@@ -12,7 +12,7 @@ jQuery(document).ready(function($){
                     documentId: documentId,
                     nonce: agbConnectorSettings.nonce,
                     fieldName: fieldName,
-                    value: value
+                    fieldValue: fieldValue
                 },
                 dataType: 'json',
                 onSuccess: function (response) {
