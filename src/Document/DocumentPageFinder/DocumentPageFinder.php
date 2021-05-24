61d53c8290ee0c5884787a6c405dffb51fd815df
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-05-24 20:50:43 +0300

    delete method for finding pages with documents

--- src/Document/DocumentPageFinder/DocumentPageFinder.php
+++ src/Document/DocumentPageFinder/DocumentPageFinder.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Inpsyde\AGBConnector\Document\DocumentPageFinder;
 
-use Inpsyde\AGBConnector\Plugin;
-
 /**
  * Service for finding pages displaying documents.
  */
@@ -50,60 +48,5 @@ class DocumentPageFinder implements DocumentFinderInterface
         }
 
         return $foundPostIds;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function findAllPostsDisplayingDocuments(): array
-    {
-        $byAllocation = $this->findAllPostsDisplayingDocumentsByAllocation();
-        $byShortcode = $this->findAllPostsDisplayingDocumentsByShortcode();
-        return array_unique(array_merge($byAllocation, $byShortcode));
-    }
-
-    /**
-     * Find the pages displaying documents selected from the old plugin settings.
-     *
-     * @return int[]
-     */
-    protected function findAllPostsDisplayingDocumentsByAllocation(): array
-    {
-        $allocations = get_option(Plugin::OPTION_TEXT_ALLOCATIONS, []);
-
-        $found = [];
-
-        foreach ($allocations as $allocationsOfType){
-            $foundPortion = array_column($allocationsOfType, 'pageId');
-            $found = array_merge($found, $foundPortion);
-        }
-
-        return $found;
-    }
-
-    protected function findAllPostsDisplayingDocumentsByShortcode(): array
-    {
-        $foundPosts = [];
-        $args = [
-            'numberposts' => '-1',
-            'fields' => 'ids',
-            'post_status' => 'publish'
-        ];
-
-        foreach($this->shortcodes as $shortcode){
-            $args['s'] = $shortcode;
-
-            $postsWithShortcodeText = get_posts($args);
-
-            $postsWithShortcode = array_filter($postsWithShortcodeText, function ($postId) use ($shortcode){
-                $postContent = get_post_field('post_content', $postId);
-                return has_shortcode($postContent, $shortcode);
-            });
-
-
-            $foundPosts = array_merge($foundPosts, $postsWithShortcode);
-        }
-
-        return $foundPosts;
     }
 }
